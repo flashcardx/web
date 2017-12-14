39b6a8094cc4e59eafb1a178e08138392c3abc9d
pablo marino <pablo-n-m@hotmail.com>
2017-12-13 23:50:29 -0300

    mobile modal now displays on middle of screen

--- src/components/util/modal.jsx
+++ src/components/util/modal.jsx
@@ -14,11 +14,8 @@ const style = {
          transform: 'translate(-50%, -50%)'
     },
     mobileStyles:{
-        position: "fixed",
+        position: 'absolute',
         width: "100%",
-        minHeight: "100%",
-        margin: 0,
-        padding: 0,
         left: '50%',
         top: '50%',
         transform: 'translate(-50%, -50%)'
