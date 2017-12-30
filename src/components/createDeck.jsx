e5e527b23a3d88ba6be64abbdd579d93d06fd663
pablo marino <pablo-n-m@hotmail.com>
2017-12-30 15:21:31 -0300

    less text in create deck tooltip test

--- src/components/createDeck.jsx
+++ src/components/createDeck.jsx
@@ -137,7 +137,7 @@ class CreateDeck extends Component{
                                                     />
                                                 </div>
                                                 <div className="col-2 col-sm-2 col-md-1">
-                                                    <Tooltip title="El lenguaje en el que vas a crear los titulos de las fichas adentro de este mazo, <br/> es importante ya que otros servicios como por ejemplo la &quot;pronunciacion automatica&quot; se basan en este campo. <br/>Si experimentas problemas relacionados al idioma puede que tengas que cambiar este campo">
+                                                    <Tooltip title="El lenguaje en el que vas a crear los titulos de las fichas adentro de este mazo, es importante ya que otros servicios como por ejemplo la &quot;pronunciacion automatica&quot; se basan en este campo.">
                                                         <i className="fa fa-info-circle" style={{margin:"5px", bottom:4, position:"absolute"}} aria-hidden="true"></i>
                                                     </Tooltip>
                                                 </div>
