6b12228eff123cfa8b8dd48afa518a90ae37c79b
pablo marino <pablo-n-m@hotmail.com>
2018-01-04 00:44:08 -0300

    fixed eslint complaint

--- src/reducers/reducer_translator.js
+++ src/reducers/reducer_translator.js
@@ -22,11 +22,11 @@ export function translatePreferencesReducer(state={}, action){
                                     newState[action.deckId].to = obj.to;
                                     newState[action.deckId].from = obj.from;
                                     return newState;
-        case UPDATE_TRANSLATE_PREFERENCES_TO: var newState = _.clone(state);
-                                              if(!newState[action.deckId])
-                                                 newState[action.deckId] = {}
-                                              newState[action.deckId].to = action.payload;
-                                                return newState;
+        case UPDATE_TRANSLATE_PREFERENCES_TO: var newState3 = _.clone(state);
+                                              if(!newState3[action.deckId])
+                                                 newState3[action.deckId] = {}
+                                              newState3[action.deckId].to = action.payload;
+                                                return newState3;
         case UPDATE_TRANSLATE_PREFERENCES_FROM: var newState2 = _.clone(state);
                                                 if(!newState2[action.deckId])
                                                     newState2[action.deckId] = {}
